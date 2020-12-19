5715ce1984b4f78350892e5d63d83aec41eee746
YOUR NAME <mariem.chalghaf1992@gmail.com>
2020-12-19 16:15:50 +0100

    first commit

---
+++ js/main2.js
@@ -0,0 +1,34 @@
+let heart=document.getElementById('heart')
+let heartt=document.getElementById('heartt')
+let hearttt=document.getElementById('hearttt')
+  
+//condition smaill
+function heart1(){
+   if(heart.style.color=='red'){
+     heart.style.color='black'
+   }
+   else{
+     heart.style.color='red'
+   }
+ }
+
+ function heart2(){
+   if(heartt.style.color=='red'){
+      heartt.style.color='black'
+   }
+   else{
+     heartt.style.color='red'
+   }
+ }
+
+ function heart3(){
+   if(hearttt.style.color=='red'){
+     hearttt.style.color='black'
+   }
+   else{
+     hearttt.style.color='red'
+   }
+ }
+
+
+
